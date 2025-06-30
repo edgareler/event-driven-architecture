82aae85de30ccf03bfa13b203825ba6f5a16e2da
Edgar Eler <eler@edgar.systems>
2025-06-30 17:11:56 -0300

    Create initial frontend configuration

---
+++ frontend/eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      'max-len': ['error', { code: 80 }],
+      strict: ['error', 'global'],
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
+]);
